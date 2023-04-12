7228d85fe90de8825bc21ee2b9eed1754685c79a
QuangHaii <hainoob2002@gmail.com>
2023-04-12 14:46:06 +0700

    name3

--- Entity/Class1.cs
+++ Entity/Class1.cs
@@ -7,5 +7,7 @@
         public string id { get; set; }
         public string name2 { get; set; }
         public int age2 { get; set; }
+        public string id2 { get; set; }
+        public string name3 { get; set; }
     }
 }
