6ce89570256d724c7d894fec9c6940deb62c4eb2
YangTris <you@example.com>
2023-04-12 14:58:53 +0700

    Push class234

---
+++ Entity/Class234.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    internal class Class234
+    {
+        public int nem { get; set; }
+        public int nem2 { get; set;}
+
+        public DateTime dateOfBirth { get; set; }
+        public string gmail { get; set; }
+        public string phoneNumber { get; set; }
+    }
+}
