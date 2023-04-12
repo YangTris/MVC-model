04872e464bfc6cdf0bc3162807458783b842f861
QuangHaii <hainoob2002@gmail.com>
2023-04-12 15:29:49 +0700

    chotri

--- DataAccess/Class1.cs
+++ DataAccess/Class1.cs
@@ -6,6 +6,6 @@
         public string co_ho_hai { get; set;}
         public int conchotringu { get; set; }
         public string co_ho_hai_100000 { get; set;}
-
+        public string conchotringu2 { get; set; }
     }
 }
