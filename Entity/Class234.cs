ec41ebadc7215572a34b778cd1a254dc1d85a45f
QuangHaii <hainoob2002@gmail.com>
2023-04-12 15:12:29 +0700

    my property

--- Entity/Class234.cs
+++ Entity/Class234.cs
@@ -11,10 +11,12 @@ namespace Entity
         public int nem { get; set; }
         public int nem2 { get; set;}
         public DateTime date { get; set; }
-
         public int nem3 { get; set;}
         public int nem4 { get; set;}
         public int nem5 { get; set;}
         public DateTime DOB { get; set; }
+        public int MyProperty { get; set; }
+        public int MyProperty2 { get; set;}
+        public int MyProperty3 { get; set;}
     }
 }
