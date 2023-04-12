dd5d9600b7793a12d1de2b9518ad88e3760f7959
QuangHaii <hainoob2002@gmail.com>
2023-04-12 14:49:45 +0700

    123

---
+++ Entity/Class234.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    internal class Class234
+    {
+        public int nem { get; set; }
+        public int nem2 { get; set;}
+    }
+}
