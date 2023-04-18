b1cb0d76fc5986ce332478b285066b598e289a0e
QuangHaii <hainoob2002@gmail.com>
2023-04-18 16:33:05 +0700

    update user

---
+++ Entity/WebUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    public class WebUser
+    {
+        public string loginID {  get; set; }
+        public string password { get; set; }
+        public UserState state { get; set; }
+    }
+}
