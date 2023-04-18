6d8e05f39dad9c65f26ab327a7ed3ab116d0105d
QuangHaii <hainoob2002@gmail.com>
2023-04-18 16:58:03 +0700

    update order line

---
+++ Entity/OrderLine.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    public class OrderLine
+    {
+        public string orderLineID {  get; set; }
+        public string orderID { get; set; }
+        public string productID { get; set; }
+        public double price { get; set; }
+        public int quantity { get; set; }
+
+    }
+}
