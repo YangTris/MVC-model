04517eca9d4c740013e4b53ba883dd3a6c846b76
QuangHaii <hainoob2002@gmail.com>
2023-04-22 14:20:27 +0700

    product controller + viewmodel

---
+++ MVC-model/Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_model.Models;
+using Service;
+
+namespace MVC_model.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductService _productService;
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public IActionResult Index()
+        {
+            var model = _productService.GetAll().Select(product => new IndexProductViewModel
+            {
+                productID = product.productID,
+                name = product.name,
+                brand = product.brand,
+                description = product.description,
+                price = product.price,
+                quantity = product.quantity,
+                imgURL = product.imgURL,
+                inDate = product.inDate,
+            }).ToList();
+            return View(model);
+        }
+    }
+}
