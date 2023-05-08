067cf9ac4ad74ef8d148c494a38ac7d815b8a24d
YangTris <you@example.com>
2023-05-08 12:09:10 +0700

    add UserView

--- MVC-model/Controllers/UserController.cs
+++ MVC-model/Controllers/UserController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MVC_model.Models;
+using Service;
 
 namespace MVC_model.Controllers
 {
@@ -8,9 +10,33 @@ namespace MVC_model.Controllers
         {
             return View();
         }
+  
+        private IProductService _productService;
+        private IWebHostEnvironment _webHostEnvironment;
+        public UserController(IProductService productService, IWebHostEnvironment webHostEnvironment)
+        {
+            _productService = productService;
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        [HttpGet]
         public IActionResult Product()
         {
-            return View();
+            var model = _productService.GetAll().Select(product => new IndexProductViewModel
+            {
+                productID = product.productID,
+                productName = product.productName,
+                brand = product.brand,
+                price = product.price,
+                category = product.category,
+                imgURL = product.imgURL,
+                discountPercentage = product.discountPercentage,
+                /*created_at = product.created_at,
+                modified_at = product.modified_at,
+                deleted_at = product.deleted_at,
+                create_by = product.create_by,*/
+            }).ToList();
+            return View(model);
         }
     }
 }
