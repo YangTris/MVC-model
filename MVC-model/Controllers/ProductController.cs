926fe0e677c851b8f62c83af5b4607086864bf90
YangTris <you@example.com>
2023-07-27 22:51:41 +0700

    alert Login before purchase

--- MVC-model/Controllers/ProductController.cs
+++ MVC-model/Controllers/ProductController.cs
@@ -8,7 +8,7 @@ using Service;
 using System.Security.Claims;
 using PagedList;
 using Microsoft.AspNetCore.Authorization;
-
+using Microsoft.AspNetCore.Identity;
 namespace MVC_model.Controllers
 {
     
@@ -142,6 +142,12 @@ namespace MVC_model.Controllers
             {
                 return NotFound();
             }
+            /*bool isAuthenticated = User.Identity.IsAuthenticated;
+            if (!isAuthenticated)
+            {
+                
+                return NotFound();
+            }*/
             var user = User.FindFirst(ClaimTypes.NameIdentifier).Value;
             var model = new CartIndexProductViewModel
             {
@@ -260,113 +266,8 @@ namespace MVC_model.Controllers
             }
             return View(model);
         }
-        /*List<CartItem> GetCartItems()
-        {
 
-            var session = HttpContext.Session;
-            string jsoncart = session.GetString(CARTKEY);
-            if (jsoncart != null)
-            {
-                return JsonConvert.DeserializeObject<List<CartItem>>(jsoncart);
-            }
-            return new List<CartItem>();
-        }
-        void ClearCart()
-        {
-            var session = HttpContext.Session;
-            session.Remove(CARTKEY);
-        }
-        void SaveCartSession(List<CartItem> ls)
-        {
-            var session = HttpContext.Session;
-            string jsoncart = JsonConvert.SerializeObject(ls);
-            session.SetString(CARTKEY, jsoncart);
-        }
 
-        [Route("addcart/{productid:int}", Name = "addcart")]
-        public IActionResult AddToCart([FromRoute] int productid)
-        {
-
-            var product = _productService.GetById(productid);
-            if (product == null)
-                return NotFound("Không có sản phẩm");
-
-            // Xử lý đưa vào Cart ...
-            var cart = GetCartItems();
-            var cartitem = cart.Find(p => p.product.productID == productid);
-            if (cartitem != null)
-            {
-                // Đã tồn tại, tăng thêm 1
-                cartitem.quantity++;
-            }
-            else
-            {
-                //  Thêm mới
-                cart.Add(new CartItem() { quantity = 1, product = product });
-            }
-
-            // Lưu cart vào Session
-            SaveCartSession(cart);
-            // Chuyển đến trang hiện thị Cart
-            return RedirectToAction(nameof(Cart));
-        }
-
-        [Route("/cart", Name = "cart")]
-        public IActionResult Cart()
-        {
-            return View(GetCartItems());
-        }
-
-        [Route("/updatecart", Name = "updatecart")]
-        [HttpPost]
-        public IActionResult UpdateCart([FromForm] int productid, [FromForm] int quantity)
-        {
-            // Cập nhật Cart thay đổi số lượng quantity ...
-            var cart = GetCartItems();
-            var cartitem = cart.Find(p => p.product.productID == productid);
-            if (cartitem != null)
-            {
-                // Đã tồn tại, tăng thêm 1
-                cartitem.quantity = quantity;
-            }
-            SaveCartSession(cart);
-            // Trả về mã thành công (không có nội dung gì - chỉ để Ajax gọi)
-            return Ok();
-        }
-        [Route("/removecart/{productid:int}", Name = "removecart")]
-        public IActionResult RemoveCart([FromRoute] int productid)
-        {
-            var cart = GetCartItems();
-            var cartitem = cart.Find(p => p.product.productID == productid);
-            if (cartitem != null)
-            {
-                // Đã tồn tại, tăng thêm 1
-                cart.Remove(cartitem);
-            }
-
-            SaveCartSession(cart);
-            return RedirectToAction(nameof(Cart));
-        }
-
-        /*[HttpGet]
-        public IActionResult CartIndex()
-        {
-            var model = _productService.GetAll().Select(product => new CartIndexProductViewModel
-            {
-                productID = product.productID,
-                productName = product.productName,
-                brand = product.brand,
-                price = product.price,
-                category = product.category,
-                imgURL = product.imgURL,
-                discountPercentage = product.discountPercentage,
-                /*created_at = product.created_at,
-                modified_at = product.modified_at,
-                deleted_at = product.deleted_at,
-                create_by = product.create_by,
-            }).ToList();
-            return View(model);
-        }*/
 
     }
 }
